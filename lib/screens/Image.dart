514a9613502599b32758211f1318867337e4e5ca
Malak925 <Malakdawod925@gmail.com>
2022-12-20 00:12:55 +0300

    third step

---
+++ lib/screens/Image.dart
@@ -0,0 +1,44 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart';
+
+class ImageView extends StatelessWidget {
+  final NetworkImage pic;
+  const ImageView({super.key, required this.pic});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Container(
+            child: Image(image: pic),
+          ),
+          IconButton(
+              onPressed: () async {
+                var response = await http.get(Uri.parse(pic.url));
+                Directory? documentDirectory =
+                    await getApplicationDocumentsDirectory();
+                File file = new File(
+                    path.join(documentDirectory!.path, path.basename(pic.url)));
+                await file.writeAsBytes(response.bodyBytes);
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        content: Image.file(file),
+                      );
+                    });
+
+                print(file);
+              },
+              icon: Icon(Icons.download))
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/details.dart
+++ lib/screens/details.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:poptoknow/screens/Image.dart';
 
 class Details extends StatelessWidget {
   final List people;
@@ -14,36 +15,98 @@ class Details extends StatelessWidget {
       appBar: AppBar(
         title: Text(people[index]['name']),
       ),
-      body: ListView(
-        // ignore: sort_child_properties_last
-        children: <Widget>[
-          Image(
-              //       // ignore: prefer_interpolation_to_compose_strings
-              image: NetworkImage(
-                  "https://image.tmdb.org/t/p/original${people[index]['profile_path']}")),
-          Text("Popular Movies"),
-          // Text(knownFor[0]["original_title"]),
-          Text(knownFor[0]['overview']),
-          // Text(knownFor[0]["original_title"]),
-          // if (knownFor[0]['poster_path'] != null)
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: ListView(
+          shrinkWrap: true,
+          physics: BouncingScrollPhysics(),
+          // ignore: sort_child_properties_last
+          children: [
+            Image(
+                //       // ignore: prefer_interpolation_to_compose_strings
+                image: NetworkImage(
+                    "https://image.tmdb.org/t/p/original${people[index]['profile_path']}")),
+            Text(people[index]['gender'] == 2
+                ? "Gender: Male"
+                : "Gender: Female"),
+            Text("Popularity: ${people[index]['popularity']}"),
+            Text(
+              "Popular Movies",
+              style: TextStyle(fontSize: 30),
+            ),
+            Text(
+              knownFor[0]["original_title"] != null
+                  ? knownFor[0]["original_title"]
+                  : "Text NOT Found",
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              knownFor[0]['overview'],
+            ),
+            Text(
+              knownFor[1]["original_title"] != null
+                  ? knownFor[1]["original_title"]
+                  : "Text NOT Found",
+              style: TextStyle(fontSize: 20),
+            ),
 
-          Container(
-            color: Colors.blue,
-            child: Text(knownFor[0]['overview']),
-          ),
+            Container(
+              color: Colors.blue,
+              child: Text(
+                knownFor[1]['overview'],
+              ),
+            ),
 
-          // Text(knownFor[0]["original_title"]),
-          Container(
-            color: Colors.red,
-            child: Text(knownFor[0]['overview']),
-          ),
-          // Container(
-          //   child: Image(
-          //       image: NetworkImage("https://image.tmdb.org/t/p/w500" +
-          //           knownFor[1]['backdrop_path'])),
-          // )
-        ],
-        scrollDirection: Axis.vertical,
+            Text(
+              knownFor[2]["original_title"] != null
+                  ? knownFor[2]["original_title"]
+                  : "Text NOT Found",
+              style: TextStyle(fontSize: 20),
+            ),
+            Container(
+              color: Colors.red,
+              child: Text(
+                knownFor[2]['overview'],
+              ),
+            ),
+            // Container(
+            //   child: Image(
+            // image: NetworkImage("https://image.tmdb.org/t/p/w500" +
+            //     knownFor[1]['backdrop_path'])),
+            // )
+
+            Container(
+              height: 200,
+              width: 200,
+              child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                      mainAxisSpacing: 0,
+                      crossAxisSpacing: 5),
+                  itemCount: 3,
+                  itemBuilder: (context, index) {
+                    NetworkImage picV = NetworkImage(
+                        "https://image.tmdb.org/t/p/w500" +
+                            knownFor[index]['poster_path']);
+                    return InkWell(
+                        onTap: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: ((context) {
+                            return ImageView(pic: picV);
+                          })));
+                        },
+                        child: Container(
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(image: picV),
+                          ),
+                        ));
+                  }),
+            )
+          ],
+        ),
       ),
     );
   }
